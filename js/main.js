9609d0869636d5fad9e05dfa4dcb83f146073ed6
khamzat-abd <yourgitemail@example.com>
2023-02-12 17:31:38 +0300

    add: js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close");
-console.log(modalToggle);
-modalToggle.forEach((element) => {
-  element.addEventListener("click", (event) => {
+const modalContainer = document.querySelector(".modal-container");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalContainer) &&
+      modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    modal.classList.add("is-open");
-  });
-  modalClose.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.classList.remove("is-open");
-  });
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
 });
